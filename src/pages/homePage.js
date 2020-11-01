7d182aa94b787fc79d4da7d8b8d44d4a9f4c99ff
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2020-11-01 22:58:20 +0000

    Adding routing and live data form API

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
-const MovieListPage = ({movies}) => {
-  return (
-    <>
-      <Header numMovies={movies.length} />
-      <FilterControls />
-      <MovieList movies={movies} />
-    </>
-  );
+const MovieListPage = () => {
+    const [titleFilter, setTitleFilter] = useState("");       // NEW
+    const [genreFilter, setGenreFilter] = useState("0");
+
+    const [movies, setMmovies] = useState([]);
+    useEffect(() => {
+        fetch(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+        )
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                return json.results
+            })
+            .then(movies => {
+                setMmovies(movies);
+            });
+    }, []);
+    const genre = Number(genreFilter)
+    let displayedMovies = movies
+        .filter(m => {
+            return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
+        })
+        .filter(m => {
+            return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
+        });
+
+    const handleFilterChange = (type, value) => {
+        if (type === "name") setTitleFilter(value);
+        else setGenreFilter(value);
+    };
+    return (
+        <>
+            <Header numMovies={displayedMovies.length} />
+            <FilterControls onUserInput={handleFilterChange} />
+            <MovieList movies={displayedMovies} />
+        </>
+    );
 };
 
 export default MovieListPage;
