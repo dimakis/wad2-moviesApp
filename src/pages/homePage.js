b5a4160c35fe7bb6c3f7ff4ed4518b5554196614
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2020-11-02 00:23:00 +0000

    Trying to fix error with 'setMovies' not defined

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
+import StubAPI from "../api/stubAPI";
 
 const MovieListPage = () => {
-    const [titleFilter, setTitleFilter] = useState("");       
+    const [titleFilter, setTitleFilter] = useState("");
     const [genreFilter, setGenreFilter] = useState("0");
 
     const [movies, setMmovies] = useState([]);
@@ -21,6 +22,7 @@ const MovieListPage = () => {
                 setMmovies(movies);
             });
     }, []);
+
     const genre = Number(genreFilter)
     let displayedMovies = movies
         .filter(m => {
@@ -32,13 +34,34 @@ const MovieListPage = () => {
 
     const handleFilterChange = (type, value) => {
         if (type === "name") setTitleFilter(value);
-        else setGenreFilter(value);
+        else setGenreFilter(value); const addToFavorites = movieId => {
+            // Find index position of selected movie in the list
+            const index = movies.map(m => m.id).indexOf(movieId)
+
+            StubAPI.add(movies[index])
+            const updatedList = [...movies]  // Make a copy of the movie list
+            updatedList.splice(index, 1)     // Remove selected movie from home page list
+            setMovies(updatedList)
+        }
+
     };
-    return (
+
+    const addToFavorites = movieId => {
+        // Find index position of selected movie in the list
+        const index = movies.map(m => m.id).indexOf(movieId)
+    
+        StubAPI.add(movies[index])
+        const updatedList = [...movies]  // Make a copy of the movie list
+        updatedList.splice(index, 1)     // Remove selected movie from home page list
+        setMovies(updatedList)  
+      } 
+      
+      return (
         <>
             <Header numMovies={displayedMovies.length} />
             <FilterControls onUserInput={handleFilterChange} />
-            <MovieList movies={displayedMovies} />
+            <MovieList movies={displayedMovies}
+                buttonHandler={addToFavorites} />
         </>
     );
 };
