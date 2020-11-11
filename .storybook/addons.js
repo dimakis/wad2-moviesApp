a12cb010d210f2df501c27b20fc6fe087e560e3a
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2020-11-11 00:23:44 +0000

    fix storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,3 +2,7 @@ module.exports = {
     addons: ['@storybook/addon-actions/register',
     '@storybook/preset-create-react-app']
   }
+
+  module.exports = {
+    addons: ['@storybook/addon-actions/register']
+  }
